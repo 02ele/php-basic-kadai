6cd2524e44ffa628c082c7d07b9dc46559383e49
haruka.s <105984955+02ele@users.noreply.github.com>
2023-10-23 14:09:15 +0900

    課題７章アップデート

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="utf-8">
+  <title>課題７章</title>
+</head>
+
+<body>
+  <p>
+    <?php
+      // 連想配列に値を代入する
+      $stock = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+
+      // 連想配列の値を出力する
+      print_r($stock);
+
+    ?>
+  </p>
+
+</body>
